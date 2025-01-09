4c2cde6161bf6f2b5f3905415a83065148bbd42c
thuc.nguyen1@cgi.com <thuc.nguyen1@cgi.com>
2025-01-09 15:15:51 -0400

    add models

--- django_inventory_tracker/settings.py
+++ django_inventory_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myrestaurant.apps.MyrestaurantConfig'
 ]
 
 MIDDLEWARE = [
